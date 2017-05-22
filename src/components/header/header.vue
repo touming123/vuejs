d1f39d7fa7bb0e94351dd8c72aad627a0187d197
touming123 <junlijiaojiao@163.com>
2017-05-22 20:18:57 +0800

    detail and star components

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -32,17 +32,45 @@
     <div class="background">
       <img :src="seller.avatar" />
     </div>
+    <transition name="fade">
     <div class="detail" v-show="detailShow">
       <div class="detail-wrapper clearfix">
-        <div class="detail-main"></div>
+        <div class="detail-main">
+          <div class="name">{{seller.name}}</div>
+          <div class="star-wrapper">
+            <star :size="48" :score="seller.score"></star>
+          </div>
+          <div class="tittle">
+            <div class="line"></div>
+            <div class="text">优惠信息</div>
+            <div class="line"></div>
+          </div>
+          <ul v-if="seller.supports" class="supports">
+            <li v-for="item in seller.supports" class="support-item">
+              <span class="icon" :class="classMap[item.type]"></span>
+              <span class="text">{{item.description}}</span>
+            </li>
+          </ul>
+          <div class="tittle">
+            <div class="line"></div>
+            <div class="text">商家公告</div>
+            <div class="line"></div>
+          </div>
+          <div class="bulletin">
+            <p class="content">{{seller.bulletin}}</p>
+          </div>
+        </div>
       </div>
       <div class="detail-close" @click="hideDetail">
         <i class="icon-close"></i>
       </div>
     </div>
+    </transition>
   </div>
 </template>
 <script type="text/ecmascript-6">
+  import star from 'components/star/star.vue';
+
   export default {
     props: {
       seller: {
@@ -61,6 +89,12 @@
       hideDetail() {
         this.detailShow = false;
       }
+    },
+    components: {
+      star
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
     }
   };
 </script>
@@ -185,15 +219,80 @@
     top: 0
     left: 0
     overflow: hidden
-    background-color: rgba(7, 17, 27, 0.5)
-    
+    transition: all 2.5s
+    backdrop-filter: blur(10px)
+    opacity: 1
+    background: rgba(7, 17, 27, 0.8)
+    &.fade-enter-active, &.fade-leave-active
+      transition: all 0.5s
+    &.fade-enter, &.fade-leave-active
+      opacity: 0
+      background: rgba(7, 17, 27, 0)
     .detail-wrapper
       width: 100%
       min-height: 100%
       .detail-main
         margin-top: 64px
         padding-bottom: 64px // sticky footer，需要配合清除浮动
-    
+        .name
+          line-height: 16px
+          text-align: center
+          font-size: 16px
+          font-weight: 700
+        .star-wrapper
+          margin-top: 18px
+          padding: 2px 0
+          text-align: center
+        .tittle
+          display: flex
+          width: 80%
+          margin: 30px auto
+          .line
+            flex: 1
+            position: relative
+            top: -6px
+            border-bottom: 1px solid rgba(255, 255, 255, 0.2)
+          .text
+            padding: 0 12px
+            font-size: 14px
+            font-weight: 700
+        .supports
+          width: 80%
+          margin: 0 auto
+          .support-item
+            font-size: 0
+            padding: 0 12px
+            margin-bottom: 12px
+            &:last-child
+              margin-bottom: 0
+            .icon
+              display: inline-block
+              width: 16px
+              height: 16px
+              margin-right: 6px
+              vertical-align: top
+              background-size: 16px 16px
+              background-repeat: no-repeat
+              &.decrease
+                bg-image('decrease_2')
+              &.discount
+                bg-image('discount_2')
+              &.guarantee
+                bg-image('guarantee_2')
+              &.invoice
+                bg-image('invoice_2')
+              &.special
+                bg-image('special_2')
+            .text
+              line-height: 16px
+              font-size: 12px
+        .bulletin
+          width: 80%
+          margin: 0 auto
+          .content
+            padding: 0 12px
+            line-height: 24px
+            font-size: 12px
     .detail-close
       position: relative
       width: 32px
